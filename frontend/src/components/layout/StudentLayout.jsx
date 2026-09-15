3ef0f7030cde3a8eecfdede8a0790df7a187503c
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-09-15 13:52:43 +0300

    add StudentLayout component

---
+++ frontend/src/components/layout/StudentLayout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Sidebar from './Sidebar'
+
+const StudentLayout = ({ children }) => (
+  <div className='min-h-screen bg-gray-50'>
+    <Navbar />
+    <Sidebar />
+    <div className='lg:pl-64 pt-16'>
+      <main className='min-h-[calc(100vh-4rem)]'>{children}</main>
+    </div>
+  </div>
+)
+
+export default StudentLayout
