753e8f8332443076314cd78b12fbb0f9f6cd45f1
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-09-15 13:53:49 +0300

    add AdminLayout component

--- frontend/src/components/layout/TeacherLayout.jsx
+++ frontend/src/components/layout/TeacherLayout.jsx
@@ -1,17 +1,15 @@
-import React from 'react';
-import Navbar from './Navbar';
-import Sidebar from './Sidebar';
+import React from 'react'
+import Navbar from './Navbar'
+import Sidebar from './Sidebar'
 
 const TeacherLayout = ({ children }) => (
-  <div className="min-h-screen bg-gray-50">
+  <div className='min-h-screen bg-gray-50'>
     <Navbar />
     <Sidebar />
-    <div className="lg:pl-64 pt-16">
-      <main className="min-h-[calc(100vh-4rem)]">
-        {children}
-      </main>
+    <div className='lg:pl-64 pt-16'>
+      <main className='min-h-[calc(100vh-4rem)]'>{children}</main>
     </div>
   </div>
-);
+)
 
-export default TeacherLayout;
+export default TeacherLayout
